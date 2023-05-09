6ab65172a6eacfa7b6702b3b94cfe510fe9a040e
thalgi <thalgi.muller@gmail.Com>
2023-05-09 17:11:05 +0200

    Ajout Mailing fonctionnel basé sur le Repo POC-MAIL-Python

--- api/User.py
+++ api/User.py
@@ -3,7 +3,10 @@ import uuid
 
 from flask_restful import Resource, reqparse
 
-from mail.mail import send_email_with_qr_code
+from mail.mail import init_mail_sender, send_email_with_qrcode
+
+
+service = init_mail_sender()
 
 users = {}
 
@@ -17,6 +20,7 @@ class User:
         self.email = email
         self.password = self.hash_password(password)
         self.access_token = secrets.token_hex(32)
+        self.validate = False
 
     @staticmethod
     def hash_password(password):
@@ -35,11 +39,16 @@ class User:
             'access_token': self.access_token
         }
 
-
     def to_dict(self):
         return {
             'username': self.username,
             'email': self.email,
+        }
+
+    def to_uuid(self):
+        return {
+            'username': self.username,
+            'uuid': self.uuid,
         }
 
 
@@ -54,7 +63,7 @@ class CreateUser(Resource):
         new_user = User(args['username'], args['email'], args['password'])
         users[new_user.uuid] = new_user
 
-        return {'message': 'User created successfully', 'user': new_user.to_dict_full()}, 201
+        return {'message': 'User created successfully', 'user': new_user.to_uuid()}, 201
 
 
 class GetUser(Resource):
@@ -82,7 +91,7 @@ class UpdateUser(Resource):
         if args['email'] is not None:
             user.email = args['email']
 
-        return {'message': 'User updated successfully', 'user': user.to_dict()}
+        return {'message': 'User updated successfully', 'user': user.to_dict()},201
 
 
 class DeleteUser(Resource):
@@ -110,16 +119,55 @@ class GetUserToken(Resource):
         if user is None:
             return {'error': 'Invalid email or password'}, 401
 
-        return {'access_token': user.access_token}
+        return {'access_token': user.access_token}, 201
+
+
+class GetUserUuid(Resource):
+    def post(self):
+        parser = reqparse.RequestParser()
+        parser.add_argument('email', type=str, required=True, help='Email is required and must be a string.')
+        parser.add_argument('password', type=str, required=True, help='Password is required and must be a string.')
+        args = parser.parse_args()
+
+        user = None
+        for u in users.values():
+            if u.email == args['email']:
+                user = u
+                if u.check_password(args['password']):
+                    if user.validate:
+                        break
+                    else:
+                        return {'error': 'The User is not validated'}, 401
+
+        if user is None:
+            return {'error': 'Invalid email or password'}, 401
+
+        return {'user_uuid': user.uuid}, 201
 
 
 class ValidateUser(Resource):
-    def post(self, user_id):
+    def get(self, user_id):
         if user_id not in users:
             return {'error': 'User not found'}, 404
 
         user = users[user_id]
-        qr_data = "Never Gonna Give You Up"
-        send_email_with_qr_code(user.email, qr_data)
 
-        return {'message': 'QR code email sent successfully'}
+        to_email = str(user.email)
+        qr_data = "https://7695-2a01-cb19-d81-c600-dd62-6606-ee27-e4bb.ngrok-free.app/confirm_user" + str(user.uuid)
+        send_email_with_qrcode(service, to_email, qr_data)
+
+        return {'message': 'QR code email sent successfully'}, 201
+
+
+class CheckMailExist(Resource):
+    def post(self):
+        parser = reqparse.RequestParser()
+        parser.add_argument('email', type=str, required=True, help='Email input is required and must be a string.')
+        args = parser.parse_args()
+        email = args['email']
+
+        for u in users.values():
+            if email == u.email:
+                return {'message': 'The email exists.'}
+            else:
+                return {'message': "The email doesn't exist."}
